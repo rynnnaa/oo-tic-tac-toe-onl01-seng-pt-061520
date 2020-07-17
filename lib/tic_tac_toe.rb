2ad9654ae11fbaad72a3a68038400d41737a5f04
Ryna Truong <rynatruong@outlook.com>
2020-07-17 20:12:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,8 +5,9 @@ class TicTacToe
       [0,3,6], [1,4,7], [2,5,8],
       [0,4,8], [2,4,6]
       ]
-  def initialize
-    @board = [" ", " ", " ", " "," ", " ", " ", " ", " "]
+  
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
   end
   
   def display_board
@@ -17,23 +18,31 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
-  def input_to_index(input)
-    input.to_i-1
+  def input_to_index(user_input)
+    user_input.to_i - 1
   end
   
   def move(index, token)
     @board[index] = token
   end
   
-  def position_taken?(input)
-    @board[input] != " "
+  def position_taken?(user_input)
+    @board[user_input] != " "
   end
   
   def valid_move?(index)
-    !position_taken?(index) && index.between? 8)
+    !position_taken?(index) && index.between?(0,8)
   end
   
- def turn 
+  def turn_count
+    @board.count {|box| box != " "}
+  end
+  
+  def current_player
+      turn_count.even? ? "X" : "O"
+  end
+  
+  def turn 
     puts "Please enter number from (1-9):"
     input = gets.strip
     index = input_to_index(input)
@@ -46,14 +55,6 @@ class TicTacToe
     display_board
   end
   
-  def turn_count
-    @board.count {|box| box != " "}
-  end
-  
-  def current_player
-    turn_count.even? ? "X" : "O"
-  end
-  
   def won?
     WIN_COMBINATIONS.any? do |match|
       if position_taken?(match[0]) && @board[match[0]] == @board[match[1]] && @board[match[1]] == @board[match[2]]
@@ -61,4 +62,27 @@ class TicTacToe
       end
     end
   end
-end
+  
+  def full?
+    @board.all?{|box| box != " "}
+  end
+  
+    def draw?
+    full? && !won?
+  end
+  
+  def over?
+    won? || draw?
+  end
+  
+  def winner
+    if match = won?
+      @board[match[0]]
+    end
+  end
+  
+  def play 
+    turn until over?
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+  end
+end 
