acc1ca473d3311ee9f7f9641d0d0a48ed88b8897
Ryna Truong <rynatruong@outlook.com>
2020-07-17 19:56:53 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,60 @@
+class TicTacToe
+  
+  WIN_COMBINATIONS = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+      ]
+  def initialize
+    @board = [" ", " ", " ", " "," ", " ", " ", " ", " "]
+  end
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input)
+    input.to_i-1
+  end
+  
+  def move(index, token)
+    @board[index] = token
+  end
+  
+  def position_taken?(input)
+    @board[input] != " "
+  end
+  
+  def valid_move?(index)
+    !position_taken?(index) && index.between?(0,8)
+  end
+  
+ def turn 
+    puts "Please enter number from (1-9):"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+    else
+      turn
+    end
+    display_board
+  end
+  
+  def turn_count
+    @board.count {|box| box != " "}
+  end
+  
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+  
+  def won?
+    
+  
+end
